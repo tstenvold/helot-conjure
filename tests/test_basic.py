5eb1932c57b7ca7d2a99f52778a1a4899e9c0de5
Terence Stenvold <tstenvold@gmail.com>
2021-09-22 13:46:44 +0200

    4 working tests with error messages imported

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,6 +2,7 @@
 
 import socket
 import pytest
+from messages import *
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 12345        # The port used by the server
@@ -20,6 +21,26 @@ class TestJsonFiles:
             s.connect((HOST, PORT))
             assert sendJsonFile(s, "tests/function.json") == "6"
             s.close()
+
+    def test_invaliduser(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            assert sendString(
+                s, '{"userName": "teste","authToken": "abc123","Code": "result = 2+2*4"}') == INVALIDAUTH
+            s.close()
+
+    def test_invalidauth(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            assert sendString(
+                s, '{"userName": "tester","authToken": " ","Code": "result = 2+2*4"}') == INVALIDAUTH
+            s.close()
+
+
+def sendString(con, text):
+    con.sendall(text.encode())
+    data = con.recv(2048)
+    return data.decode()
 
 
 def sendJsonFile(con, filePath):
